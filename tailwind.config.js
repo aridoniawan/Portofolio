7ec7b3eee20f7b73df636bd2e8b6d751da9e9adc
aridoniawan <aridoniawan1@gmail.com>
2024-11-05 16:06:43 +0700

    new update nov

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "./node_modules/flowbite/**/*.js"],
   theme: {
     container: {
       center: true,
@@ -23,6 +23,7 @@ module.exports = {
     },
   },
   plugins: [
+    require("flowbite/plugin"),
     function ({ addUtilities }) {
       addUtilities(
         {
